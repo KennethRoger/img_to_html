23ad9319187b50cf88a7d14b754b8036cf362728
KennethRoger <kennethroger11@gmail.com>
2025-05-31 12:48:10 +0530

    Added some comments for current routes (/, /upload)

--- backend/server.js
+++ backend/server.js
@@ -7,22 +7,22 @@ const { storage } = require("./utils/multerStorage");
 const upload = multer({ storage: storage });
 
 require("dotenv").config();
-
 const port = process.env.PORT;
 
 const corsOptions = {
   origin: process.env.CLIENT_URL,
 };
-
 app.use(cors(corsOptions));
 
+// static test route
 app.get("/", (req, res) => {
   res.status(200).json("Hello");
 });
 
+// Uploading route
 app.post("/upload", upload.single("image"), (req, res) => {
   const data = req.file;
-  console.log("img",data);
+  console.log("img", data);
   res
     .status(200)
     .json({ success: true, message: "Successfully recieved formData" });
