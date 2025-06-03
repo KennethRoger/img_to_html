e88beec1c21b06e52af244c3dfae73cfba3b0bac
KennethRoger <kennethroger11@gmail.com>
2025-06-03 18:16:26 +0530

    Implemented text bbox marking in image

--- backend/utils/maskText.js
+++ backend/utils/maskText.js
@@ -5,21 +5,21 @@ const path = require("node:path");
 function maskRegion(x0, y0, width, height, image) {
   x0 = Math.max(0, x0 - 5);
   y0 = Math.max(0, y0 - 5);
-  const fillColor = image.getPixelColor(x0, y0);
+  // const fillColor = image.getPixelColor(x0, y0);
   width = width + 10;
   height = height + 10;
-  // const fillColor = 0x00ffffff;
+  const fillColor = 0x00ffffff;
   for (let x = x0; x < x0 + width; ++x) {
     for (let y = y0; y < y0 + height; ++y) {
-      // if (
-      //   x === x0 ||
-      //   y === y0 ||
-      //   x === x0 + width - 1 ||
-      //   y === y0 + height - 1
-      // ) {
-      //   image.setPixelColor(fillColor, x, y);
-      // }
-      image.setPixelColor(fillColor, x, y);
+      if (
+        x === x0 ||
+        y === y0 ||
+        x === x0 + width - 1 ||
+        y === y0 + height - 1
+      ) {
+        image.setPixelColor(fillColor, x, y);
+      }
+      // image.setPixelColor(fillColor, x, y);
     }
   }
 }
@@ -27,19 +27,21 @@ function maskRegion(x0, y0, width, height, image) {
 async function maskText(file) {
   try {
     const image = await Jimp.read(file);
-    const jsonArr = await fs.readFile(
+    const bboxJsonArr = await fs.readFile(
       path.join(__dirname, "..", "data/text.JSON"),
       "utf-8"
     );
-    let data = JSON.parse(jsonArr);
-    console.log(data);
+    let bboDataArr = JSON.parse(bboxJsonArr);
 
-    const x0 = 365;
-    const y0 = 203;
-    const width = 467 - x0;
-    const height = 240 - y0;
-
-    maskRegion(x0, y0, width, height, image);
+    for (let bboxData of bboDataArr) {
+      if (bboxData.confidence > 50) {
+        let x0 = bboxData.bbox.x0;
+        let y0 = bboxData.bbox.y0;
+        let width = bboxData.bbox.x1 - x0;
+        let height = bboxData.bbox.y1 - y0;
+        maskRegion(x0, y0, width, height, image);
+      }
+    }
 
     // const res = await fs.writeFile(
     //   path.join(__dirname, "..", "data/imageData.txt"),
@@ -48,7 +50,7 @@ async function maskText(file) {
     const baseName = path.basename(file);
     await image.write(path.join(__dirname, "..", `uploads/test/${baseName}`));
     // await fs.unlink(file);
-    console.log("Written image details to file");
+    // console.log("Written image details to file");
     console.log("Written image successfully");
   } catch (err) {
     throw err;
